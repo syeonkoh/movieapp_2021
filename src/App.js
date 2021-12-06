c601be4b98453491b13c28b937f49bbbeafabeb9
syeonkoh <syeonko12@gmail.com>
2021-12-06 18:51:33 -0500

    Understand state 11

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+
 import React from "react";
 
 
-function App() {
-  return <div>hello</div>;
-}
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("hello");
+  }
+  state = {
+    isLoading: true,
+    movies: []
+  };
 
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({isLoading: false});
+    },6000);
+  }
+  
+  render(){
+    console.log("render")
+    const {isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "Ready"}</div>;
+  }
+}
 export default App;
+
